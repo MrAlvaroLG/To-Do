406ee33290c45a144a89127d10bfb8fe02d0c9d5
AlvaroLG27 <alvarolopezgomez27@gmail.com>
2024-05-13 19:10:20 -0400

    elegant popup display created by me :D

--- Frontend/popup/popup.js
+++ Frontend/popup/popup.js
@@ -1,11 +1,16 @@
 const OpenPopUp = document.querySelector("#User")
 const ClosePopUp = document.querySelector("#ClosePU")
 const PopUp = document.querySelector("#UserAdministration")
+const body = document.body;
 
 OpenPopUp.addEventListener("click", ()=>{
     PopUp.showModal();
+    body.classList.remove('blur-effect-remove');
+    body.classList.add('blur-effect');
 })
 
 ClosePopUp.addEventListener("click", ()=>{
     PopUp.close();
+    body.classList.remove('blur-effect');
+    body.classList.add('blur-effect-remove');
 })
